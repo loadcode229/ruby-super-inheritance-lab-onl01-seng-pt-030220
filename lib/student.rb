b061b6ceab9dff8631e9c62a9da9359b187608e4
Adam Priest <sepone229@gmail.com>
2020-04-03 23:44:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
   def hello
-    return "Hey there! I'm so excited to learn stuff!\n"
+    return "Hey there! I'm so excited to learn stuff!"
   end
   def raise_hand
     puts "Pick me!"
